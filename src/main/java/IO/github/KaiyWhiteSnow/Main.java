e009bf245a3b349b249e1995db3122a55e367df4
Kaiy <106684837+KaiyWhiteSnow@users.noreply.github.com>
2024-09-09 09:06:36 +0200

    Very message

---
+++ src/main/java/IO/github/KaiyWhiteSnow/Main.java
@@ -0,0 +1,71 @@
+package IO.github.KaiyWhiteSnow;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static ArrayList<Mazlicek> listMazlicku = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("Chcete přídat mazlíčka (1), vypsat informace o mazlíčkovy (2), vypsat jména mazlíčků (3), vypsat průměrný věk a váhu (4)?");
+            int akce = sc.nextInt();
+            sc.nextLine();
+            switch (akce){
+                case 1:
+                    pridatMazlicky();
+                    break;
+                case 2:
+                    vypsatInfo();
+                    break;
+                case 3:
+                    vypsatJmena();
+                    break;
+                case 4:
+                    Mazlicek.prumernaVaha(listMazlicku);
+                    Mazlicek.prumernyVek(listMazlicku);
+                    break;
+                default:
+                    throw new RuntimeException();
+            }
+        }
+    }
+
+    public static void pridatMazlicky(){
+        System.out.println("Kolik mazlíčků chcete přídat?");
+        int pocetMazlicku = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < pocetMazlicku; i++) {
+            System.out.println("Zadejte jméno, druh, věk a váhu mazlíčka: ");
+            String jmenoMazlicka = sc.nextLine();
+            String druhMazlicka = sc.nextLine();
+            int vekMazlicka = sc.nextInt();
+            sc.nextLine();
+            float vahaMazlicka = sc.nextFloat();
+            sc.nextLine();
+            Mazlicek mazlicek = new Mazlicek(jmenoMazlicka, druhMazlicka, vekMazlicka, vahaMazlicka);
+            listMazlicku.add(mazlicek);
+        }
+    }
+
+    public static void vypsatInfo() {
+        System.out.println("O kterém mazlíčkovy chcete vypsat info?");
+        for (int i = 1; i <= listMazlicku.size(); i++) {
+            System.out.println(i + ": " + listMazlicku.get(i-1).getJmeno());
+        }
+        int mazlicek = sc.nextInt();
+        System.out.println(listMazlicku.get(mazlicek-1).toString());
+    }
+
+    public static void vypsatJmena() {
+        for (int i = 0; i < listMazlicku.size(); i++) {
+            if (i == listMazlicku.size()-1) {
+                System.out.println(listMazlicku.get(i).getJmeno() + ".");
+            }
+            else {
+                System.out.println(listMazlicku.get(i).getJmeno() + ", ");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/IO/github/KaiyWhiteSnow/Mazlicek.java
@@ -0,0 +1,62 @@
+package IO.github.KaiyWhiteSnow;
+
+import java.util.ArrayList;
+
+public class Mazlicek {
+    String jmeno;
+    String druh;
+    int vek;
+    float vaha;
+
+    public String getJmeno() {
+        return jmeno;
+    }
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+    public String getDruh() {
+        return druh;
+    }
+    public void setDruh(String druh) {
+        this.druh = druh;
+    }
+    public int getVek() {
+        return vek;
+    }
+    public void setVek(int vek) {
+        this.vek = vek;
+    }
+    public float getVaha() {
+        return vaha;
+    }
+    public void setVaha(float vaha) {
+        this.vaha = vaha;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Jméno: %s, Druh: %s, Věk: %d, Váha: %f", jmeno, druh, vek, vaha);
+    }
+
+    public static int prumernyVek(ArrayList<Mazlicek> listMazlicku) {
+        int celkovyVek = 0;
+        for (Mazlicek mazlicek : listMazlicku) {
+            celkovyVek += mazlicek.getVek();
+        }
+        return celkovyVek / listMazlicku.size();
+    }
+
+    public static float prumernaVaha(ArrayList<Mazlicek> listMazlicku) {
+        float celkovaVaha = 0.f;
+        for (Mazlicek mazlicek : listMazlicku) {
+            celkovaVaha += mazlicek.getVaha();
+        }
+        return celkovaVaha / listMazlicku.size();
+    }
+    public Mazlicek(String jmeno, String druh, int vek, float vaha) {
+        this.jmeno = jmeno;
+        this.druh = druh;
+        this.vek = vek;
+        this.vaha = vaha;
+    }
+}
